ad04e9bf26e41142a991a7d0105d4b6e17349859
cocowool <cocowool@gmail.com>
2024-07-17 17:05:32 +0800

    modify code

---
+++ LightProxy/LightProxy.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class LightProxy {
+    public static void main(String[] args){
+        int localPort = 8989;
+        
+    }
+}
